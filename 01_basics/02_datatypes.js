3120757e2d04803d851e190e72bd7e39e911eed7
KaranKhandekar1729 <145787469+KaranKhandekar1729@users.noreply.github.com>
2024-05-20 08:57:12 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js as new js version
+
+//alert(3 + 3); //we are using nodejs not browser
+
+console.log(3 + 
+    3) //readability is must
+
+console.log("Karan")
+
+let name = "Karan"
+let age = 19
+let isLogged = true
+let state;
+
+//Primitive
+// number => 2 to the power 35
+// bigint
+// string = ""
+// boolean => true/false
+// undefined
+// null => standalone value
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
