268957de8c23338599c9b8e1ab6bc74c34650dcd
KaranKhandekar1729 <145787469+KaranKhandekar1729@users.noreply.github.com>
2024-05-20 10:24:00 +0000

    compare

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); //equality operator and comparison treat null as differently
+// here at (1) > converts null to num treating it as 0 threfore it is false, while
+// >= at (3) is true
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// === (strict check) - checks the val as well as datatype
+
+console.log("2" === 2);
