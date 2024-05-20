783812e3b711e34e71372129ad986c3e1f99d645
KaranKhandekar1729 <145787469+KaranKhandekar1729@users.noreply.github.com>
2024-05-20 09:33:23 +0000

    Conversions of Datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,29 @@
+let score = "28abc"
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+let isNumber = Number(score)
+// console.log(typeof isNumber) //number
+// console.log(isNumber); //NaN
+
+// "28" => 28
+// "28abc" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = "Karan"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// "" => false
+// "Karan" => true
+// "1" => true, "0" => false
+
+let someNumber = 11
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
