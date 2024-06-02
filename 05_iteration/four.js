ba6fc4249b417355a7ea74ce168a875b8d572849
KaranKhandekar1729 <145787469+KaranKhandekar1729@users.noreply.github.com>
2024-06-02 13:35:46 +0000

    forin

---
+++ 05_iteration/four.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} is an abbreviation for ${myObject[key]}`);
+}
+
+const Language = ['js', 'cpp', 'rb', 'swift']
+
+for (const key in Language) {
+    console.log(Language[key]);
+}
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
