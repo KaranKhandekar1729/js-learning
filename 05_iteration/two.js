a6fff0e7880d560b8f77881c7ddc13dad0d349fd
KaranKhandekar1729 <145787469+KaranKhandekar1729@users.noreply.github.com>
2024-06-02 10:13:00 +0000

    while / dowhile

---
+++ 05_iteration/two.js
@@ -0,0 +1,24 @@
+// while (true) {
+    
+// }
+
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+const myarr = ["thor", 'batman', 'superman']
+let arr = 0
+while (arr < myarr.length) {
+    console.log(myarr[arr]);
+    arr = arr + 1
+}
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score<=10);
